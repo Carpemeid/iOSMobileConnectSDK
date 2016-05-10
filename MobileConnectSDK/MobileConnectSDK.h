8be9f4bad4bf739b9775e6b27177748950e6cfa8
Dan Andoni <d.andoni@rosoftlab.net>
2016-05-10 15:35:16 +0100

    Add MobileConnectSDK.h file

---
+++ MobileConnectSDK/MobileConnectSDK.h
@@ -0,0 +1,19 @@
+//
+//  MobileConnectSDK.h
+//  MobileConnectSDK
+//
+//  Created by Andoni Dan on 10/05/16.
+//  Copyright © 2016 bjss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileConnectSDK.
+FOUNDATION_EXPORT double MobileConnectSDKVersionNumber;
+
+//! Project version string for MobileConnectSDK.
+FOUNDATION_EXPORT const unsigned char MobileConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileConnectSDK/PublicHeader.h>
+
+
